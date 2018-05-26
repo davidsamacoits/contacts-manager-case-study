6659769e1e1e8982b579f24a3996c0b994d66590
David <david@samacoits.com>
2018-05-26 13:45:19 -0400

    Updated directory feature

--- src/features/Directory/directory.js
+++ src/features/Directory/directory.js
@@ -1,16 +1,63 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import ContactCard from '../../components/cards/ContactCard';
+import LetterDivider from '../../components/dividers/LetterDivider';
+
+import './style.css';
+
+const propTypes = {
+  contacts: PropTypes.array.isRequired,
+  typeCard: PropTypes.string.isRequired,
+  consoleLog: PropTypes.func.isRequired,
+  changeTypeCard: PropTypes.func.isRequired,
+};
 
 const Directory = (props) => {
-  // console.log('>>>>>>>> props', props);
   const {
+    contacts,
+    typeCard,
     consoleLog,
+    changeTypeCard,
   } = props;
   consoleLog();
+
+  let currentLetter = '';
+
+  const renderLetterDivider = (letter) => {
+    if (letter !== currentLetter) {
+      currentLetter = letter;
+      return (
+        <LetterDivider letter={currentLetter} />
+      );
+    }
+    return null;
+  };
+
+  const renderContacts = () => contacts.map(item => ([
+    renderLetterDivider(item.lastName.charAt(0)),
+    <ContactCard
+      firstName={item.firstName}
+      lastName={item.lastName}
+      picture={item.picture}
+      id={item.id}
+      jobTitle={item.jobTitle}
+      className="contacts-container__card"
+      type={typeCard}
+    />,
+  ]));
+
   return (
-    <p>
-      Directory
-    </p>
+    <div className="directory">
+      <button onClick={() => changeTypeCard('SMALL')}>SMALL</button>
+      <button onClick={() => changeTypeCard('BIG')}>BIG</button>
+      <div className="contacts-container">
+        {renderContacts()}
+      </div>
+    </div>
   );
 };
+
+Directory.propTypes = propTypes;
 
 export default Directory;
--- src/features/Directory/index.js
+++ src/features/Directory/index.js
@@ -3,13 +3,16 @@ import { connect } from 'react-redux';
 import Directory from './directory';
 
 import { consoleLog } from '../../services/contactService/actions';
+import { changeTypeCard } from '../../services/directoryService/actions';
 
 function mapStateToProps(state) {
   return {
     contacts: state.contactReducer.contacts,
+    typeCard: state.directoryReducer.typeCard,
   };
 }
 
 export default connect(mapStateToProps, {
   consoleLog: () => consoleLog(),
+  changeTypeCard: type => changeTypeCard(type),
 })(Directory);
