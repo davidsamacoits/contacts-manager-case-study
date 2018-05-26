cc668914ccde82da6663ff19a7532b4e02bc9d28
David <david@samacoits.com>
2018-05-26 16:05:21 -0400

    Added order for contacts

--- src/services/directoryService/reducer.js
+++ src/services/directoryService/reducer.js
@@ -1,8 +1,11 @@
-import { CARD_TYPES } from '../../components/cards/ContactCard/constants';
 import * as actionTypes from './actionTypes';
+
+import { CARD_TYPES } from '../../components/cards/ContactCard/constants';
+import { ORDER } from '../../constants';
 
 export const initialState = () => ({
   typeCard: CARD_TYPES.BIG,
+  order: ORDER.ASC,
 });
 
 export default (state = initialState(), action) => {
